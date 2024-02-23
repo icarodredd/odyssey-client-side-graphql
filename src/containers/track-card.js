84acc274fd1cc0cb26eaac6611fc18b975b9497f
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-02-22 21:37:25 -0700

    add text decoration none

--- src/containers/track-card.js
+++ src/containers/track-card.js
@@ -74,6 +74,7 @@ const CardContainer = styled(Link)({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
+  textDecoration: 'none',
   [mq[0]]: {
     width: '90%',
   },
